1a36d40b24ac750ac4179802f5a44420a5787d2d
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-08-09 01:23:19 +0100

    Update task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,4 @@
--- Script that creates a view need_meeting that lists all students
--- that have a score under 80 (strict) and no last_meeting or more than 1 month.
-CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
-AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
+-- script that ranks country origins of bands, ordered
+-- by the number of (non-unique) fans
+SELECT origin, SUM(fans) as nb_fans from metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
